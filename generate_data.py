6d8e4c41b0942b6606e42f29095baa06402acccc
HHalva <halva.hermanni@gmail.com>
2020-09-18 16:33:10 +0300

    generate_data DONE

--- generate_data.py
+++ generate_data.py
@@ -22,7 +22,6 @@ def gen_source_data(num_comp, num_latent_states, sequence_length,
     radius -- controls overlap of different states' distributions (default=2)
     random_seed -- for reproducible stochasticity (default=0)
     """
-
     # set random see
     np.random.seed(random_seed)
 
@@ -46,16 +45,6 @@ def gen_source_data(num_comp, num_latent_states, sequence_length,
     ev_one = np.real(e_vecs[:, one_eig_idx])
     init_state_probs = ev_one / ev_one.sum()
 
-    # create latent state sequence
-    state_sequence = np.zeros(sequence_length, dtype=np.int)
-    for i in range(sequence_length):
-        if i == 0:
-            m_draw = np.random.multinomial(1, pvals=init_state_probs)
-        else:
-            m_draw = np.random.multinomial(
-                1, pvals=transition_matrix[state_sequence[i-1], :])
-        state_sequence[i] = np.argmax(m_draw)
-
     # create mean modulation parameters for different latent states
     # that are sampled far away from each other on an n-sphere
     means = sample_distant_nsphere_points(num_comp, num_latent_states)
@@ -64,14 +53,15 @@ def gen_source_data(num_comp, num_latent_states, sequence_length,
     # also ensure good condition number
     dists = means.repeat(num_latent_states, 0)-np.tile(
         means, (num_latent_states, 1))
-    pdb.set_trace()
     dists = np.sqrt((dists**2).sum(1))
     min_dist = np.min(dists[dists.nonzero()])
     cond_num_best = 1e+9
     for i in range(1000):
-        sigmasq = np.random.uniform(0.01, min_dist/1.65,
-                                     size=(num_latent_states, num_comp))
-        sigmasq = sigmasq**(1/num_comp) / num_latent_states
+        # idea that in worst case only 5% of distrib A is under
+        # 95% of distrib B and vice versa
+        sigmasq = np.random.uniform(0.01, min_dist/(2*1.65),
+                                    size=(num_latent_states, num_comp))
+        # write in natural parameter form
         natural_params_linear = means / sigmasq
         natural_params_square = -1. / sigmasq
         natural_params = np.zeros(shape=(num_latent_states, 2 * num_comp))
@@ -84,10 +74,20 @@ def gen_source_data(num_comp, num_latent_states, sequence_length,
             cond_num_best = cond_num
             sigmasq_best = sigmasq.copy()
     sigmasq = sigmasq_best
-    D = np.zeros(shape=(num_latent_states, num_comp, num_comp),
-                  dtype=np.float64)
+    D = np.zeros(shape=(num_latent_states, num_comp, num_comp))
+    pdb.set_trace()
     for k in range(num_latent_states):
         D[k] = np.diag(sigmasq[k])
+
+    # create latent state sequence
+    state_sequence = np.zeros(sequence_length, dtype=np.int)
+    for i in range(sequence_length):
+        if i == 0:
+            m_draw = np.random.multinomial(1, pvals=init_state_probs)
+        else:
+            m_draw = np.random.multinomial(
+                1, pvals=transition_matrix[state_sequence[i-1], :])
+        state_sequence[i] = np.argmax(m_draw)
 
     # generate HMM data for independent components (sources)
     source_data = np.zeros((sequence_length, num_comp))
